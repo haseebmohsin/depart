9fc3804f1d2b81f89e9283b62542305f504fafa6
haseeb11634 <haseeb11634@gmail.com>
2023-02-25 18:07:29 +0500

    database connection problem

--- config/database.php
+++ config/database.php
@@ -46,11 +46,11 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => parse_url(env('DATABASE_URL'), '127.0.0.1'),
+            'port' => parse_url(env('DATABASE_URL'), '3306'),
+            'database' => substr(parse_url(env('DATABASE_URL'), 'forge'), 1),
+            'username' => parse_url(env('DATABASE_URL'), 'forge'),
+            'password' => parse_url(env('DATABASE_URL'), ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
@@ -123,7 +123,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
